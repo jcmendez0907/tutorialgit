1496eff598c40e8fbcd89a823f9252be32242ffe
Juan Cruz Mendez <jcmendez@192.168.1.7>
2023-01-09 08:49:04 -0700

    Edicion ayb
    SE edita la linea 1 de A y la toma como nueva linea
    Se agrega entrelinea archivo b

--- commit/a.js
+++ commit/a.js
@@ -1,1 +1,2 @@
-console.log("Linea 01 de a.js");
+console.log("A-Linea 01 de a.js");
+console.log("A-Linea 02 de a.js");
--- commit/b.js
+++ commit/b.js
@@ -1,3 +1,4 @@
 console.log("B-Linea 01 de b.js");
+console.log("B-Linea 011 de b.js");
 console.log("B-Linea 02 de b.js");
 console.log("B-Linea 03 de b.js");
